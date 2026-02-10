3bfcf437b9914ad068f5f32092a61c5028e61300
songcs0329 <songcs0329@gmail.com>
2026-02-11 02:13:10 +0900

    방 공유하기 기능 추가

--- rummikub-front/src/pages/Room.tsx
+++ rummikub-front/src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { Navigate, useParams } from 'react-router';
 import { useRoomEvents } from '@/hooks/useRoomEvents';
 import { useCustomerStore } from '@/store/useCustomerStore';
 import PlayersContainer from '@/components/pages/room/PlayersContainer';
+import ShareRoom from '@/components/pages/room/ShareRoom';
 
 function Room() {
   const { roomCode } = useParams();
@@ -12,24 +13,20 @@ function Room() {
 
   if (!customer) return <Navigate to={`/?roomCode=${roomCode}`} replace />;
 
-  if (!room) return <div>room loading...</div>;
+  if (!room) return null;
 
-  if (room?.gameStarted) {
+  if (room.gameStarted) {
     // 게임 시작됐을 때 랜더링할 컴포넌트
     return <div></div>;
   }
 
   // 게임 대기 때 랜더링할 컴포넌트
   return (
-    <div>
-      <h1>Room</h1>
-      <div>{roomCode}</div>
-      <div>{JSON.stringify(customer)}</div>
-      {room && (
-        <div className="flex flex-col gap-y-2 bg-red-100">
-          <PlayersContainer players={room.players} />
-        </div>
-      )}
+    <div className="flex min-h-screen items-center justify-center p-4">
+      <div className="flex w-full max-w-2xl flex-col gap-y-4">
+        <ShareRoom roomCode={roomCode!} />
+        <PlayersContainer players={room.players} />
+      </div>
     </div>
   );
 }
