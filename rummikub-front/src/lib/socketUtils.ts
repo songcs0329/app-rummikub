a72d4285eb8ff14fb636f2a1c1208cb49c04164d
songcs0329 <songcs0329@gmail.com>
2026-02-11 01:03:04 +0900

    socketUtils.ts 수정

--- rummikub-front/src/lib/socketUtils.ts
+++ rummikub-front/src/lib/socketUtils.ts
@@ -2,11 +2,18 @@ import { io, Socket } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_APP_API_URL || 'http://localhost:3000';
 
-export const createSocket = (): Socket => {
-  return io(SOCKET_URL, {
-    autoConnect: false,
-    transports: ['websocket'],
-  });
+let socket: Socket | null = null;
+
+export const getSocket = (): Socket => {
+  if (!socket) {
+    socket = io(SOCKET_URL, {
+      autoConnect: false,
+      transports: ['websocket'],
+    });
+  }
+  return socket;
 };
+
+export const createSocket = getSocket;
 
 export type { Socket };
