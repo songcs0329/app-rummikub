e045d0d40b5746885ed8a0d5b09fc2a264a291bb
songcs0329 <songcs0329@gmail.com>
2026-02-10 16:39:47 +0900

    chore: reset front workspace

--- rummikub-front/src/App.tsx
+++ rummikub-front/src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from '@/pages/Home';
-import Todos from '@/pages/Todos';
-import TodoDetail from '@/pages/Todos/TodoDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="/todos/:todoId" element={<TodoDetail />} />
       </Routes>
     </BrowserRouter>
   );
