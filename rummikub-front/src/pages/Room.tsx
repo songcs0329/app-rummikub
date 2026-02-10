35c6b81bbf6a1e8b294d24606c061981817cf06a
songcs0329 <songcs0329@gmail.com>
2026-02-11 01:07:40 +0900

    Room 전역 상태, 소켓 이벤트 구독 훅 추가

--- rummikub-front/src/pages/Room.tsx
+++ rummikub-front/src/pages/Room.tsx
@@ -1,5 +1,5 @@
 import { Navigate, useParams } from 'react-router';
-import { useFindRoom } from '@/hooks/useFindRoom';
+import { useRoomEvents } from '@/hooks/useRoomEvents';
 import { useCustomerStore } from '@/store/useCustomerStore';
 
 function Room() {
@@ -7,7 +7,8 @@ function Room() {
 
   const customer = useCustomerStore((state) => state.customer);
 
-  useFindRoom(roomCode);
+  const { room } = useRoomEvents(roomCode);
+  console.log('useRoomEvents ===>', room);
 
   if (!customer) return <Navigate to={`/?roomCode=${roomCode}`} replace />;
 
@@ -16,6 +17,17 @@ function Room() {
       <h1>Room</h1>
       <div>{roomCode}</div>
       <div>{JSON.stringify(customer)}</div>
+      {room && (
+        <div className="flex flex-col gap-y-2">
+          {room.players.map((player) => {
+            return (
+              <div key={player.id} className="border border-red-500">
+                {JSON.stringify(player)}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
