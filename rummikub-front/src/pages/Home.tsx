0dfe8629b3e7489f590b974ba5695c8fe4067296
songcs0329 <songcs0329@gmail.com>
2026-02-10 16:54:54 +0900

    라이브러리 추가: zod, RHF, shadcn/ui

--- rummikub-front/src/pages/Home.tsx
+++ rummikub-front/src/pages/Home.tsx
@@ -1,5 +1,20 @@
+/**
+ * TODO:
+ * 루미큐브 방 생성 페이지
+ * https://zod.dev/
+ * https://react-hook-form.com/
+ *
+ */
+
+import { Button } from '@/components/ui/button';
+
 function Home() {
-  return <div>home page</div>;
+  return (
+    <div>
+      <h1>home page</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
 export default Home;
--- rummikub-front/vite.config.ts
+++ rummikub-front/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -20,7 +20,7 @@ export default ({ mode }: { mode: string }) => {
     },
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@': path.resolve(__dirname, './src'),
       },
     },
   });
